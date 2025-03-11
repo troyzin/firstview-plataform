389e8eef3e913b84f1f50a5f2a5603428eb8bd36
troyzin <69919788+troyzin@users.noreply.github.com>
2025-03-11 00:55:46 -0300

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -103,7 +103,7 @@ const Auth = () => {
             <img 
               src="/logo.png" 
               alt="Logo da empresa" 
-              className="h-5 w-auto"
+              className="h-10 w-auto"
             />
           </div>
           <p className="text-gray-400">
